15778d9dccafd573e52db7bc8c434b92ab4ce029
Frank Su <frank.su@applydigital.co>
2023-05-30 14:56:40 -0700

    Add aside layout and grid content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,1 @@
-<script lang="ts">
-	import { onDestroy, onMount } from "svelte"
-
-    onMount(() => console.log('Home page mount')) 
-    onDestroy(() => console.log('Home page destroy'))
-</script>
-<nav>
-    <ul>
-        <li>
-            <a href="/">Home</a>
-        </li>
-        <li>
-            <a href="/posts">Posts</a>
-        </li>
-    </ul>
-</nav>
-
 <h1>Home page</h1>
--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -1,19 +1,5 @@
-<script lang="ts">
-	import { onDestroy, onMount } from "svelte"
-
-    onMount(() => console.log('Posts page mount')) 
-    onDestroy(() => console.log('Posts page destroy'))
-</script>
-
-<nav>
-    <ul>
-        <li>
-            <a href="/">Home</a>
-        </li>
-        <li>
-            <a href="/posts">Posts</a>
-        </li>
-    </ul>
-</nav>
-
 <h1>Posts page</h1>
+
+<p>
+    You can <a href="/posts/create">+create</a> or edit your posts here
+</p>
