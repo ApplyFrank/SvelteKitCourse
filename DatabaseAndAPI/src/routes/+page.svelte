bef362d6ada19d50243f8431e12207df84913aa7
Frank Su <frank.su@applydigital.co>
2023-06-06 13:17:47 -0700

    Add showing page data using client side rendering

--- DatabaseAndAPI/src/routes/+page.svelte
+++ DatabaseAndAPI/src/routes/+page.svelte
@@ -1,4 +1,31 @@
 <script lang="ts">
+    import type { Post } from '@prisma/client'
+
+    async function getPosts() {
+        const response = await fetch('api/posts')
+        const posts: Post[] = await response.json()
+        return posts
+    }
+</script>
+
+<h1>Posts</h1>
+
+{#await getPosts()}
+    <p>Loading...</p>
+{:then posts}
+    <p>Showing {posts.length} posts.</p>
+    {#each posts as { slug, title}} 
+        <ul>
+            <li>
+                <a href="/posts/{slug}">{title}</a>
+            </li>
+        </ul>
+    {/each}
+{:catch error}
+    <p>{error.message}</p>
+{/await}
+
+<!-- <script lang="ts">
     async function subscribe(event: Event) {
         const form = event.target as HTMLFormElement 
         const data = new FormData(form)
@@ -14,4 +41,4 @@
 <form on:submit|preventDefault={subscribe}>
     <input type="email" name="email">
     <button>Subscribe</button>
-</form>
+</form> -->
